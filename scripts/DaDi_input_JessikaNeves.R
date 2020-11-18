48208fb34c0e50a40ae45da0e77bfeff5e84cf42
JMNeves <neves.jmm@gmail.com>
2020-11-18 12:56:51 -0300

    Add files via upload

---
+++ scripts/DaDi_input_JessikaNeves.R
@@ -0,0 +1,179 @@
+########################
+###                  ###
+###   DartR - DADI   ###
+### By Jessika Neves ###
+###   November 2020  ###
+###                  ###
+########################
+##
+##Producing a dataset to run DaDi
+##As the analysis requeires one dataset per species, it is needed to filter by species before the other filters
+#
+library(dartR)
+library(radiator)
+##
+############################################
+############ Mugil brevirostris ############
+############################################
+##
+##Keeping the population
+brevirostris <- gl.keep.pop(dart, mono.rm = FALSE, pop.list=c("brev_SA"))
+##
+##Filtering
+##
+#Filter 0% missing data
+brevirostris_0MD <- gl.filter.callrate(brevirostris, method = "loc", threshold = 1.0, mono.rm = F, recalc = F, plot = F, v = 2)
+#Filter on reproducibility
+brevirostris_rep<-gl.filter.repavg(brevirostris_0MD, t=0.97)
+#Filter to remove all but one of multiple snps in the same fragment
+brevirostris_snp<-gl.filter.secondaries(brevirostris_rep, method="best") #filters out loci after ordering the genlight object on based on repeatability, avgPIC in that order (method="best") or at random (method="random")
+#Fitler by monomorphic loci 
+brevirostris_mono<-gl.filter.monomorphs(brevirostris_snp, v=5) #v=verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
+#Filter by HWE
+brevirostris_hwe<-gl.filter.hwe(brevirostris_mono, alpha = 0.05, basis = "any", bon = TRUE, v = 5)
+#Filter by paralogs
+brevirostris_trim<- gl.filter.hamming(brevirostris_hwe, threshold=0.2, pb=T, v=5)
+##
+##Convert to VCF using radiator package
+##
+vcf_brevirostris_0MD<-genomic_converter(brevirostris_trim, strata = NULL, output = "vcf",
+                                        filename = "brevirostris_0MD", parallel.core = parallel::detectCores() - 1,
+                                        verbose = TRUE)
+vcf_brevirostris_0MD$tidy.data
+tidy_brevirostris_0MD<-vcf_brevirostris_0MD$tidy.data
+write.matrix(tidy_brevirostris_0MD,"~/dartr/brevirostris/tidy_brevirostris_0MD.txt")
+##
+##
+############################################
+############    Mugil curema    ############
+############################################
+##
+##Keeping the population
+curema <- gl.keep.pop(dart, mono.rm = FALSE, pop.list=c("cure_SA", "inci_SA"))
+##
+##Filtering
+##
+#Filter 0% missing data
+curema_0MD <- gl.filter.callrate(curema, method = "loc", threshold = 1.0, mono.rm = F, recalc = F, plot = F, v = 2)
+#Filter on reproducibility
+curema_rep<-gl.filter.repavg(curema_0MD, t=0.97)
+#Filter to remove all but one of multiple snps in the same fragment
+curema_snp<-gl.filter.secondaries(curema_rep, method="best") #filters out loci after ordering the genlight object on based on repeatability, avgPIC in that order (method="best") or at random (method="random")
+#Fitler by monomorphic loci 
+curema_mono<-gl.filter.monomorphs(curema_snp, v=5) #v=verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
+#Filter by HWE
+curema_hwe<-gl.filter.hwe(curema_mono, alpha = 0.05, basis = "any", bon = TRUE, v = 5)
+#Filter by paralogs
+curema_trim<- gl.filter.hamming(curema_hwe, threshold=0.2, pb=T, v=5)
+##
+##Convert to VCF using radiator package
+##
+vcf_curema_0MD<-genomic_converter(curema_trim, strata = NULL, output = "vcf",
+                                        filename = "curema_0MD", parallel.core = parallel::detectCores() - 1,
+                                        verbose = TRUE)
+vcf_curema_0MD$tidy.data
+tidy_curema_0MD<-vcf_curema_0MD$tidy.data
+write.matrix(tidy_curema_0MD,"~/dartr/curema/tidy_curema_0MD.txt")
+##
+##
+############################################
+############   Mugil curvidens  ############
+############################################
+##
+##Keeping the population
+curvidens <- gl.keep.pop(dart, mono.rm = FALSE, pop.list=c("curv_SA", "curv_MB"))
+##
+##Filtering
+##
+#Filter 0% missing data
+curvidens_0MD <- gl.filter.callrate(curvidens, method = "loc", threshold = 1.0, mono.rm = F, recalc = F, plot = F, v = 2)
+#Filter on reproducibility
+curvidens_rep<-gl.filter.repavg(curvidens_0MD, t=0.97)
+#Filter to remove all but one of multiple snps in the same fragment
+curvidens_snp<-gl.filter.secondaries(curvidens_rep, method="best") #filters out loci after ordering the genlight object on based on repeatability, avgPIC in that order (method="best") or at random (method="random")
+#Fitler by monomorphic loci 
+curvidens_mono<-gl.filter.monomorphs(curvidens_snp, v=5) #v=verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
+#Filter by HWE
+curvidens_hwe<-gl.filter.hwe(curvidens_mono, alpha = 0.05, basis = "any", bon = TRUE, v = 5)
+#Filter by paralogs
+curvidens_trim<- gl.filter.hamming(curvidens_hwe, threshold=0.2, pb=T, v=5)
+##
+##Convert to VCF using radiator package
+##
+vcf_curvidens_0MD<-genomic_converter(curvidens_trim, strata = NULL, output = "vcf",
+                                        filename = "curvidens_0MD", parallel.core = parallel::detectCores() - 1,
+                                        verbose = TRUE)
+vcf_curvidens_0MD$tidy.data
+tidy_curvidens_0MD<-vcf_curvidens_0MD$tidy.data
+write.matrix(tidy_curvidens_0MD,"~/dartr/curvidens/tidy_curvidens_0MD.txt")
+##
+##
+############################################
+############  Mugil rubrioculus ############
+############################################
+##
+##Keeping the population
+rubrioculus <- gl.keep.pop(dart, mono.rm = FALSE, pop.list=c("rubr_SA"))
+##
+##Filtering
+##
+#Filter 0% missing data
+rubrioculus_0MD <- gl.filter.callrate(rubrioculus, method = "loc", threshold = 1.0, mono.rm = F, recalc = F, plot = F, v = 2)
+#Filter on reproducibility
+rubrioculus_rep<-gl.filter.repavg(rubrioculus_0MD, t=0.97)
+#Filter to remove all but one of multiple snps in the same fragment
+rubrioculus_snp<-gl.filter.secondaries(rubrioculus_rep, method="best") #filters out loci after ordering the genlight object on based on repeatability, avgPIC in that order (method="best") or at random (method="random")
+#Fitler by monomorphic loci 
+rubrioculus_mono<-gl.filter.monomorphs(rubrioculus_snp, v=5) #v=verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
+#Filter by HWE
+rubrioculus_hwe<-gl.filter.hwe(rubrioculus_mono, alpha = 0.05, basis = "any", bon = TRUE, v = 5)
+#Filter by paralogs
+rubrioculus_trim<- gl.filter.hamming(rubrioculus_hwe, threshold=0.2, pb=T, v=5)
+##
+##Convert to VCF using radiator package
+##
+vcf_rubrioculus_0MD<-genomic_converter(rubrioculus_trim, strata = NULL, output = "vcf",
+                                        filename = "rubrioculus_0MD", parallel.core = parallel::detectCores() - 1,
+                                        verbose = TRUE)
+vcf_rubrioculus_0MD$tidy.data
+tidy_rubrioculus_0MD<-vcf_rubrioculus_0MD$tidy.data
+write.matrix(tidy_rubrioculus_0MD,"~/dartr/rubrioculus/tidy_rubrioculus_0MD.txt")
+##
+##
+############################################
+############     Mugil liza     ############
+############################################
+##
+##Keeping the population
+liza <- gl.keep.pop(dart, mono.rm = FALSE, pop.list=c("liza_SA"))
+##
+##Filtering
+##
+#Filter 0% missing data
+liza_0MD <- gl.filter.callrate(liza, method = "loc", threshold = 1.0, mono.rm = F, recalc = F, plot = F, v = 2)
+#Filter on reproducibility
+liza_rep<-gl.filter.repavg(liza_0MD, t=0.97)
+#Filter to remove all but one of multiple snps in the same fragment
+liza_snp<-gl.filter.secondaries(liza_rep, method="best") #filters out loci after ordering the genlight object on based on repeatability, avgPIC in that order (method="best") or at random (method="random")
+#Fitler by monomorphic loci 
+liza_mono<-gl.filter.monomorphs(liza_snp, v=5) #v=verbosity: 0, silent or fatal errors; 1, begin and end; 2, progress log ; 3, progress and results summary; 5, full report [default 2]
+#Filter by HWE
+liza_hwe<-gl.filter.hwe(liza_mono, alpha = 0.05, basis = "any", bon = TRUE, v = 5)
+#Filter by paralogs
+liza_trim<- gl.filter.hamming(liza_hwe, threshold=0.2, pb=T, v=5)
+##
+##Convert to VCF using radiator package
+##
+vcf_liza_0MD<-genomic_converter(liza_trim, strata = NULL, output = "vcf",
+                                        filename = "liza_0MD", parallel.core = parallel::detectCores() - 1,
+                                        verbose = TRUE)
+vcf_liza_0MD$tidy.data
+tidy_liza_0MD<-vcf_liza_0MD$tidy.data
+write.matrix(tidy_liza_0MD,"~/dartr/liza/tidy_liza_0MD.txt")
+##
+##
+##Convert the files into DaDi’s format (.snp) using the python script available at https://github.com/CoBiG2/RAD_Tools/blob/master/vcf2DaDi.py. 
+#
+#################################################################################################################################################
+#
+#jessika.neves@icbs.ufal.br
---
+++ scripts/PCoA_color_JessikaNeves.R
@@ -0,0 +1,83 @@
+#################################
+###                           ###
+### Changing PCOA plot colors ###
+###      By Jessika Neves     ###
+###        November 2020      ###
+###                           ###
+#################################
+#
+#This script was developed under the dartR version 1.1.6 available at https://cran.r-project.org/src/contrib/Archive/dartR/
+#
+#Load the packages
+library(dartR)
+library(ggplot2)
+library(adegenet)
+library(directlabels)
+#
+gl.pcoa.plot <- function(glPca, data, scale=FALSE, ellipse=FALSE, p=0.95, labels="pop", hadjust=1.5, 
+                         vadjust=1, xaxis=1, yaxis=2) {
+  
+  if(class(glPca)!="glPca" | class(data)!="genlight") {
+    cat("Fatal Error: glPca and genlight objects required for glPca and data parameters respectively!\n"); stop()
+  }
+  
+  # Tidy up the parameters
+  #  if (labels=="smart") { hadjust <- 0; vadjust <- 0 }
+  
+  # Create a dataframe to hold the required scores
+  m <- cbind(glPca$scores[,xaxis],glPca$scores[,yaxis])
+  df <- data.frame(m)
+  
+  # Convert the eigenvalues to percentages
+  s <- sum(glPca$eig)
+  e <- round(glPca$eig*100/s,1)
+  
+  # Labels for the axes
+  xlab <- paste("PCoA Axis", xaxis, "(",e[xaxis],"%)")
+  ylab <- paste("PCoA Axis", yaxis, "(",e[yaxis],"%)")
+  
+  
+  # If labels = none
+  
+  if (labels == "none" | labels==FALSE) {
+    cat("Plotting points with no labels\n")
+    pop <- factor(pop(data))
+    df <- cbind(df,pop)
+    colnames(df) <- c("PCoAx","PCoAy","pop")
+    
+    # Plot
+    p <- ggplot(df, aes(x=df$PCoAx, y=df$PCoAy,colour=pop)) +
+      geom_point(shape=1, size=2,colour=c("darkorange2", "deepskyblue2", "darkviolet","darkorchid1", "deeppink2", "chartreuse3", "blue2")[pop],aes(colour=pop)) +
+      #geom_dl(aes(label=ind),method="first.points") +
+      #ggtitle(paste("PCoA Plot")) +
+      theme(axis.title=element_text(face="bold.italic",size="20", color="black"),
+            axis.text.x  = element_text(face="bold",angle=0, vjust=0.5, size=10),
+            axis.text.y  = element_text(face="bold",angle=0, vjust=0.5, size=10),
+            legend.title = element_text(colour="black", size=18, face="bold"),
+            legend.text = element_text(colour="black", size = 16, face="bold")
+      ) +
+      labs(x=xlab, y=ylab) +
+      geom_hline(yintercept=0) +
+      geom_vline(xintercept=0)+
+      theme(legend.position="left")
+    # Scale the axes in proportion to % explained, if requested
+    if(scale==TRUE) { p <- p + coord_fixed(ratio=e[yaxis]/e[xaxis]) }
+    # Add ellipses if requested
+    if(ellipse==TRUE) {p <- p + stat_ellipse(aes(colour=pop), type="norm", level=0.95)}
+  }
+  
+  return (p)
+}
+#plot PCoA
+#PCoA#
+pc_0MD <- gl.pcoa(all_0MD, nfactors=5)
+#Plot 1x2
+gl.pcoa.plot(pc_0MD,all_0MD, ellipse = FALSE, p = 0.95,labels = "none", hadjust = 1.5, vadjust = 1, xaxis = 1,yaxis = 2)+
+theme_bw()+coord_fixed(ratio = 1)
+#Plot 3x4
+gl.pcoa.plot(pc_0MD,all_0MD, ellipse = FALSE, p = 0.95,labels = "none", hadjust = 1.5, vadjust = 1, xaxis = 3,yaxis = 4)+
+theme_bw()+coord_fixed(ratio = 1)
+###############################################################################################################################
+#
+#jessika.neves@icbs.ufal.br
+
